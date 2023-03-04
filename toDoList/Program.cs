a303c6fe2986449d6f03478f43ca2466205aaaf9
MatiasMtz <4442@holbertonstudents.com>
2023-03-04 16:10:29 -0300

    csharpier fix

--- toDoList/Program.cs
+++ toDoList/Program.cs
@@ -1,61 +1,63 @@
 ﻿using System;
+
 class Program
 {
-	static void Main(string[] args)
-	{
-		bool program = true;
-		Console.Clear();
-		Task.deserializeList();
-		while (program)
-		{
-			displayMenu();
-			Console.WriteLine();
-			int command = 0;
-			
-			try {
-				command = Convert.ToInt32(Console.ReadLine());
-				switch (command) {
-					case 1:
-						Console.Clear();
-						Task.addTask();
-						break;
-					case 2:
-						Console.Clear();
-					    Task.viewAllTasks();
-						break;
-					case 3:
-						Console.Clear();
-						Task.deleteTask();
-						break;
-					case 4:
-						Task.serializeList();
-						program = false;
-						break;
-					default: 
-						Console.WriteLine("Please select an option between 1 and 4");
-						break;
-				}
-			}
-			catch (FormatException) {
+    static void Main(string[] args)
+    {
+        bool program = true;
+        Console.Clear();
+        Task.deserializeList();
+        while (program)
+        {
+            displayMenu();
+            Console.WriteLine();
+            int command = 0;
+
+            try
+            {
+                command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 1:
+                        Console.Clear();
+                        Task.addTask();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Task.viewAllTasks();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Task.deleteTask();
+                        break;
+                    case 4:
+                        Task.serializeList();
+                        program = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please select an option between 1 and 4");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
                 Console.WriteLine("Your input is not a number between 1 and 4.");
-				continue;
-            }			
-		}
-	}
+                continue;
+            }
+        }
+    }
 
-	static void displayMenu()
-	{
-		char space = (char)32;
-		Console.WriteLine();
-		Console.WriteLine("TO-DO LIST MENU\n");
-		Console.WriteLine($"{space}{space}(1) Add task");
-		Console.WriteLine($"{space}{space}(2) View all tasks");
-		Console.WriteLine($"{space}{space}(3) Delete task");
-		Console.WriteLine($"{space}{space}(4) Quit program\n");
-		Console.Write("Enter your choice: ");
-	}
+    static void displayMenu()
+    {
+        char space = (char)32;
+        Console.WriteLine();
+        Console.WriteLine("TO-DO LIST MENU\n");
+        Console.WriteLine($"{space}{space}(1) Add task");
+        Console.WriteLine($"{space}{space}(2) View all tasks");
+        Console.WriteLine($"{space}{space}(3) Delete task");
+        Console.WriteLine($"{space}{space}(4) Quit program\n");
+        Console.Write("Enter your choice: ");
+    }
 
-	
-
-	//Console.WriteLine(File.ReadAllText(fileName));
+    //Console.WriteLine(File.ReadAllText(fileName));
 }
--- toDoList/models/Task.cs
+++ toDoList/models/Task.cs
@@ -1,105 +1,109 @@
 using System;
 using System.Text.Json;
+
 public class Task
 {
-	private static List<Task> taskList = new List<Task>();
+    private static List<Task> taskList = new List<Task>();
 
-	public string taskName { get; set; }
-	public string taskDescription { get; set; }
-	public DateTime taskDueDate { get; set; }
+    public string taskName { get; set; }
+    public string taskDescription { get; set; }
+    public DateTime taskDueDate { get; set; }
 
-	public Task() {
-	}
+    public Task() { }
 
-	public static void addTask()
-	{
-		// Due date format depends on the current region.
-		Task task = new Task();
+    public static void addTask()
+    {
+        // Due date format depends on the current region.
+        Task task = new Task();
 
-		Console.WriteLine("Enter your task name: ");
-		string? taskName = Console.ReadLine();
-		while (string.IsNullOrEmpty(taskName))
-		{
-			Console.WriteLine("Task name can't be empty! Input your name once more");
-			taskName = Console.ReadLine();
-		}
-		task.taskName = taskName;
+        Console.WriteLine("Enter your task name: ");
+        string? taskName = Console.ReadLine();
+        while (string.IsNullOrEmpty(taskName))
+        {
+            Console.WriteLine("Task name can't be empty! Input your name once more");
+            taskName = Console.ReadLine();
+        }
+        task.taskName = taskName;
 
-		Console.WriteLine("Enter your task description: (optional)");
-		string? taskDescription = Console.ReadLine();
-		if (string.IsNullOrEmpty(taskDescription)) {
-			task.taskDescription = "*";
-		}
-		else {
-			task.taskDescription = taskDescription;
-		}
+        Console.WriteLine("Enter your task description: (optional)");
+        string? taskDescription = Console.ReadLine();
+        if (string.IsNullOrEmpty(taskDescription))
+        {
+            task.taskDescription = "*";
+        }
+        else
+        {
+            task.taskDescription = taskDescription;
+        }
 
-		Console.WriteLine("Enter your task due date: (DD-MM-YYYY)");
-		DateTime? taskDueDate = DateTime.Parse(Console.ReadLine());
-		while (taskDueDate == null)
-		{
-			Console.WriteLine("Task due date can't be empty! Input your task due date once more");
-			taskName = Console.ReadLine();
-		}
-		task.taskDueDate = (DateTime)taskDueDate;
+        Console.WriteLine("Enter your task due date: (DD-MM-YYYY)");
+        DateTime? taskDueDate = DateTime.Parse(Console.ReadLine());
+        while (taskDueDate == null)
+        {
+            Console.WriteLine("Task due date can't be empty! Input your task due date once more");
+            taskName = Console.ReadLine();
+        }
+        task.taskDueDate = (DateTime)taskDueDate;
 
-		taskList.Add(task);
-		Console.WriteLine();
-		Console.WriteLine("Task added successfully");
-	}
+        taskList.Add(task);
+        Console.WriteLine();
+        Console.WriteLine("Task added successfully");
+    }
 
-	public static void viewAllTasks()
-	{
-		string column0 = "TASK No.";
-		string column1 = "TASK NAME";
-		string column2 = "TASK DESCRIPTION";
-		string column3 = "TASK DUEDATE";
-		
-		Console.WriteLine($"| {column0, -10} | {column1,-50} | {column2,-50} | {column3,-20} |");
-		int taskNo = 0;
+    public static void viewAllTasks()
+    {
+        string column0 = "TASK No.";
+        string column1 = "TASK NAME";
+        string column2 = "TASK DESCRIPTION";
+        string column3 = "TASK DUEDATE";
 
-		foreach (Task task in Task.taskList)
-		{
-			Console.WriteLine($"| {taskNo, -10} | {task.taskName,-50} | {task.taskDescription,-50} | {task.taskDueDate.ToString("dd/MM/yyyy"),-20} |");
-			taskNo++;
-		}
-		Console.WriteLine();
-	}
+        Console.WriteLine($"| {column0, -10} | {column1, -50} | {column2, -50} | {column3, -20} |");
+        int taskNo = 0;
 
-	public static void deleteTask()
-	{
-		viewAllTasks();
-		Console.WriteLine("Enter the TASK No. to be deleted");
-		int? taskNo = int.Parse(Console.ReadLine());
-		while (taskNo == null)
-		{
-			Console.WriteLine("TASK No. can't be empty! Input your TASK No. once more");
-			taskNo = int.Parse(Console.ReadLine());
-		}
-		if (taskNo <= Task.taskList.Count)
-		{
-			Task.taskList.RemoveAt((int)taskNo);
-			Console.WriteLine("Task deleted successfully\n");
-		}
-		else
-		{
-			Console.WriteLine("Task not found\n");
-		}
-		viewAllTasks();
-		Console.WriteLine();
-	}
+        foreach (Task task in Task.taskList)
+        {
+            Console.WriteLine(
+                $"| {taskNo, -10} | {task.taskName, -50} | {task.taskDescription, -50} | {task.taskDueDate.ToString("dd/MM/yyyy"), -20} |"
+            );
+            taskNo++;
+        }
+        Console.WriteLine();
+    }
 
-	public static void serializeList()
-	{
-		string fileName = "MyNotes.json";
-		string jsonString = JsonSerializer.Serialize(Task.taskList);
-		File.WriteAllText(fileName, jsonString);
-	}
+    public static void deleteTask()
+    {
+        viewAllTasks();
+        Console.WriteLine("Enter the TASK No. to be deleted");
+        int? taskNo = int.Parse(Console.ReadLine());
+        while (taskNo == null)
+        {
+            Console.WriteLine("TASK No. can't be empty! Input your TASK No. once more");
+            taskNo = int.Parse(Console.ReadLine());
+        }
+        if (taskNo <= Task.taskList.Count)
+        {
+            Task.taskList.RemoveAt((int)taskNo);
+            Console.WriteLine("Task deleted successfully\n");
+        }
+        else
+        {
+            Console.WriteLine("Task not found\n");
+        }
+        viewAllTasks();
+        Console.WriteLine();
+    }
 
-	public static void deserializeList()
-	{
-		string fileName = "MyNotes.json";
-		string jsonString = File.ReadAllText(fileName);
-		Task.taskList = JsonSerializer.Deserialize<List<Task>>(jsonString)!;
-	}
+    public static void serializeList()
+    {
+        string fileName = "MyNotes.json";
+        string jsonString = JsonSerializer.Serialize(Task.taskList);
+        File.WriteAllText(fileName, jsonString);
+    }
+
+    public static void deserializeList()
+    {
+        string fileName = "MyNotes.json";
+        string jsonString = File.ReadAllText(fileName);
+        Task.taskList = JsonSerializer.Deserialize<List<Task>>(jsonString)!;
+    }
 }
